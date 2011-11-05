72a0f57459c6f3aa4007db5124711cf43b6e8d52
Seth Shoultes <seth@eventespresso.com>
2011-11-05 18:31:22 +0000

    Removed unused code

--- espresso-permissions-pro.php
+++ espresso-permissions-pro.php
@@ -113,54 +113,6 @@ function espresso_do_i_manage($event_id){
 	}
 }
 
-//Returns information about the current roles
-if (!function_exists('espresso_role_data')) {
-	function espresso_role_data($type){
-		global $wpdb;
-		$sql = "SELECT
-		ID, user_email, user_login,
-		first_name.meta_value as first_name,
-		last_name.meta_value as last_name,
-		phone_number.meta_value as phone_number,
-		wp_capabilities.meta_value as wp_capabilities ";
-		$sql .= " FROM wp_users
-			JOIN wp_usermeta AS wp_capabilities ON wp_capabilities.user_id=ID
-				AND wp_capabilities.meta_key='wp_capabilities'
-			LEFT JOIN wp_usermeta AS first_name ON first_name.user_id=ID
-				AND first_name.meta_key='first_name'
-			LEFT JOIN wp_usermeta AS last_name ON last_name.user_id=ID
-				AND last_name.meta_key='last_name'
-			LEFT JOIN wp_usermeta AS phone_number ON phone_number.user_id=ID
-				AND phone_number.meta_key='phone_number' ";
-		$sql .= " WHERE ";
-		//$sql .= " wp_capabilities.meta_value LIKE '%administrator%' OR wp_capabilities.meta_value LIKE '%espresso_event_admin%' OR wp_capabilities.meta_value LIKE '%espresso_event_manager%' ";
-		//$sql .= " ORDER BY ID";
-	
-		switch($type){
-			case 'admin_count':
-				$sql .= " wp_capabilities.meta_value LIKE '%administrator%' ";
-				$wpdb->get_results($sql);
-				return $wpdb->num_rows;
-			break;
-			case 'event_manager_count':
-				$sql .= " wp_capabilities.meta_value LIKE '%espresso_event_manager%' ";
-				$wpdb->get_results($sql);
-				return $wpdb->num_rows;
-			break;
-			case 'event_admin_count':
-				$sql .= " wp_capabilities.meta_value LIKE '%espresso_event_admin%' ";
-				$wpdb->get_results($sql);
-				return $wpdb->num_rows;
-			case 'event_group_count':
-				$sql .= " wp_capabilities.meta_value LIKE '%espresso_group_admin%' ";
-				$wpdb->get_results($sql);
-				return $wpdb->num_rows;
-			break;
-		}
-	}
-
-}
-
 //Build the  regional maanger page
 function espresso_permissions_user_groups(){
 	global $wpdb, $espresso_manager, $wp_roles;
