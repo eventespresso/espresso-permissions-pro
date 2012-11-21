48ef50ecbf85a2d6bc82ba7eac545a54ad3d4289
Darren Ethier <darren@eventespresso.com>
2012-11-21 21:50:07 +0000

    hook into filters to modify how staff get viewed, etc.  This just adds in what was already present.  I still have to work on modifying this to fit what was discussed int he ticket. [touch: 1536]

--- espresso-permissions-pro.php
+++ espresso-permissions-pro.php
@@ -403,3 +403,52 @@ function espresso_add_default_questions($user_id, $_role = false) {
 add_action('personal_options_update', 'espresso_add_default_questions');
 add_action('edit_user_profile_update', 'espresso_add_default_questions');
 add_action('user_register', 'espresso_add_default_questions');/**/
+
+/** STAFF MANAGEMENT STUFF **/
+add_filter('filter_hook_espresso_personal_cb_where', 'espresso_filter_staff_meta_box_list');
+add_filter('filter_hook_espresso_staff_config_mnu_join', 'espresso_filter_staff_table_list_join');
+add_filter('filter_hook_espresso_staff_config_mnu_where', 'espresso_filter_staff_table_list_where');
+
+function espresso_filter_staff_meta_box_list($where, $event_id) {
+	//todo: code below copied from the staff personal table list page and should be applied to the join hook as well?
+	$limitstaff = false;
+    global $espresso_manager;
+    if (function_exists('espresso_member_data')) {
+        if (function_exists('espresso_member_data') && ( espresso_member_data('role') == 'espresso_group_admin' )) {
+            if ($espresso_manager['event_manager_staff'] == "Y") {
+                $limitstaff = true;
+            }
+        } else if (function_exists('espresso_member_data') && ( espresso_member_data('role') == 'espresso_event_manager')) {
+            $limitstaff = true;
+        }
+    }
+
+	//todo: I just copied over the code that was existing in core.  This needs to be modified to fit the criteria of the ticket.
+	if (function_exists('espresso_member_data') ) {
+		$wpdb->get_results("SELECT wp_user FROM " . EVENTS_DETAIL_TABLE . " WHERE id = '" . $event_id . "'");
+		$wp_user = $wpdb->last_result[0]->wp_user !='' ? $wpdb->last_result[0]->wp_user:espresso_member_data('id');
+		$where = " WHERE ";
+		if ($wp_user == 0 || $wp_user == 1){
+			$where .= " (wp_user = '0' OR wp_user = '1') ";
+		}else{
+			$where .= " wp_user = '" . $wp_user ."' ";
+		}
+
+		return $where;
+	}
+}
+
+function espresso_filter_staff_meta_box_list_join($join) {
+	if ($limitstaff) {
+        $join = " JOIN $wpdb->users u on u.ID = p.wp_user";
+    }
+
+    return $join;
+}
+
+function espresso_filter_staff_table_list_where($where) {
+	if ( $limitstaff) {
+		$where = " WHERE p.wp_user = " . $current_user->ID;
+	}
+	return $where;
+}
